00e4af108c152d856613f8882e3582b01acf6009
Yuri <1yuricoutinho@gmail.com>
2026-09-21 11:08:37 -0300

    refactor(db): regenerate initial migration instead of incremental one

--- drizzle/0001_silky_arclight.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "profiles" DROP CONSTRAINT "profiles_coren_unique";--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "role" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "role" SET DEFAULT 'user'::text;--> statement-breakpoint
-UPDATE "profiles" SET "role" = 'user' WHERE "role" = 'nurse';--> statement-breakpoint
-DROP TYPE "public"."profile_role";--> statement-breakpoint
-CREATE TYPE "public"."profile_role" AS ENUM('user', 'admin');--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "role" SET DEFAULT 'user'::"public"."profile_role";--> statement-breakpoint
-ALTER TABLE "profiles" ALTER COLUMN "role" SET DATA TYPE "public"."profile_role" USING "role"::"public"."profile_role";--> statement-breakpoint
-ALTER TABLE "profiles" DROP COLUMN "coren";
